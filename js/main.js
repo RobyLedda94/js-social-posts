71fc90f6a4a5e4f28dbaa712712ac11fdce54de5
RobyLedda94 <robyledda01@yahoo.it>
2024-07-13 18:19:37 +0200

    [FIX] riga 90 e 96

--- js/main.js
+++ js/main.js
@@ -87,13 +87,13 @@ let container = document.getElementById('container');
             <div class="post__footer">
                 <div class="likes js-likes">
                     <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
                             <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                             <span class="like-button__label">Mi Piace</span>
                         </a>
                     </div>
                     <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                        Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
                     </div>
                 </div> 
             </div>   
